0036f8b3ee4dc415022ad6a8e884e5c6081b71c0
carlschr <cjcarlson136@gmail.com>
2020-11-11 20:19:10 -0600

    Add state twitter handles & render based on input

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const statesFullName = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
 const statesAbbrev = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL','GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'];
+const statesTwitter = ['ALPublicHealth', 'Alaska_DHSS', 'AZDHS', 'ADHPIO', 'CAPublicHealth', 'CDPHE', 'CTDPH', 'Delaware_DHSS', '_DCHealth', 'HealthyFla', 'GaDPH', 'HIgov_Health', 'IDHW', 'IDPH', 'StateHealthIN', 'IAPublicHealth', 'KDHE', 'KYHealthAlerts', 'LADeptHealth', 'MEPublicHealth', 'MDHealthDept', 'MassDPH', 'MichiganHHS', 'mnhealth', 'msdh', 'HealthyLivingMo', 'DPHHSMT', 'NEDHHS', 'DhhsNevada', 'NHPubHealth', 'NJDeptofHealth', 'NMDOH', 'nycHealthy', 'NCDHHS', 'NDDOH', 'OHdeptofhealth', 'HealthyOklahoma', 'OHAOregon', 'PAHealthDept', 'RIHEALTH', 'scdhec', 'SDDOH', 'TNDeptofHealth', 'TexasDSHS', 'UtahDepOfHealth', 'healthvermont', 'VDHgov', 'WADeptHealth', 'WV_DHHR', 'DHSWI', 'health_wyoming'];
 
 
 const covidCall = (stateName, countyName) => {
@@ -38,7 +39,6 @@ const covidCall = (stateName, countyName) => {
             cities.forEach(city => {
                 if (city.name === county) {
                     let cityFips = String(city.fips).split()
-                    console.log(cityFips.length)
                     if (cityFips.length < 5) {
                         cityFips.splice(0,0,"0")
                     }
@@ -51,5 +51,10 @@ const covidCall = (stateName, countyName) => {
             let state = response.data[0];
             console.log(state);
         };
+
+        //Renders twitter feed of the state's DOH
+        let handle = statesTwitter[statesFullName.indexOf(stateName)];
+
+        $('.twitter-container').html(`<a class="twitter-timeline" data-lang="en" data-width="300" data-height="500" data-theme="dark" href="https://twitter.com/${handle}?ref_src=twsrc%5Etfw">Tweets by ${handle}</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>`);
     });
 };
